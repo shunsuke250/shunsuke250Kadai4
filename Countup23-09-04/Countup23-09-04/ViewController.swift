5943c0914acd26153d6e40a21c2bd34815428249
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-09-04 20:28:48 +0900

    :sparkles: ボタンを押すとインクリメントした値を表示する機能を追加

--- Countup23-09-04/Countup23-09-04/ViewController.swift
+++ Countup23-09-04/Countup23-09-04/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var countNumLabel: UILabel!
+    @IBOutlet private weak var incrementButton: UIButton!
+    @IBOutlet private weak var clearButton: UIButton!
+    
+    private var countNum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupInitialUI()
     }
-
-
+    
+    @IBAction func didTapIncrementButton(_ sender: UIButton) {
+        countNum += 1
+        updateCountNumLabel()
+    }
+    
+    @IBAction func didTapClearButton(_ sender: UIButton) {
+        countNum = 0
+        updateCountNumLabel()
+    }
+    
+    private func setupInitialUI() {
+        countNumLabel.text = "\(countNum)"
+        incrementButton.setTitle("+1", for: .normal)
+        clearButton.setTitle("Clear", for: .normal)
+    }
+    
+    private func updateCountNumLabel() {
+        countNumLabel.text = "\(countNum)"
+    }
 }
-
